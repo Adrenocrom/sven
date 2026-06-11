bde3369a9a5d8926c45292b156de6f1ebf70bb5a
Josef Schulz <j.schulz@alaun.de>
2026-06-11 09:08:38 +0200

    U

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from sven.task import run_prompt_sequence
 
 if __name__ == "__main__":
     # You can change the model name or tweak options here
-    #interactive_chat(model="gpt-oss:20b")
+    interactive_chat(model="gpt-oss:20b")
     # Or you can use a prompt sequence to run multiple tasks in sequence
-    run_prompt_sequence(prompts=[
-        "create a the possibility to load a number ob prompt sequences from an md file",
-        "check if you persist its results",
-        "is there anything you forgot?"
-        ],model="gemma4:12b")
+    #run_prompt_sequence(prompts=[
+    #    "create a the possibility to load a number ob prompt sequences from an md file",
+    #    "check if you persist its results",
+    #    "is there anything you forgot?"
+    #    ],model="gemma4:12b")
 
--- src/sven/tools/listfiles.py
+++ src/sven/tools/listfiles.py
@@ -9,5 +9,4 @@ def listfiles() -> str:
         list of files in the current directory
     """
     result = subprocess.run([ "find", "." ], capture_output=True, text=True)
-    print(result.stdout)
     return result.stdout
