9b179c116170cf89840c83047207391ba037444a
Josef Schulz <j.schulz@alaun.de>
2026-06-11 12:25:21 +0200

    start the sprint

--- src/sven/tools/git/commit.py
+++ src/sven/tools/git/commit.py
@@ -10,7 +10,8 @@ def commit(message: str) -> dict:
         None
     """
     try:
-        result = subprocess.run(["git", "commit", "-am", f"\"{message}\"", filepath], capture_output=True, text=True)
+        print("commit")
+        result = subprocess.run(["git", "commit", "-am", message], capture_output=True, text=True)
         return {"success": True, "message": "OK", "data": result.stdout}
     except Exception as e:
         return {"success": False, "message": str(e), "data": None}
