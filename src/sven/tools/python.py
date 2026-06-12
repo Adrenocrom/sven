397a4b07a8d473643113321f2d194c922625990a
Josef Schulz <j.schulz@alaun.de>
2026-06-12 14:08:11 +0200

    u

--- src/sven/tools/python.py
+++ src/sven/tools/python.py
@@ -18,3 +18,18 @@ def compilefiles() -> dict:
         return {"success": True, "message": "OK", "data": results}
     except Exception as e:
         return {"success": False, "message": str(e), "data": None}
+
+def compilefile(filepath: str) -> dict:
+    """
+    Compiles the file and output the compiler warnings.
+
+    args:
+        str: filepath
+    return:
+        str: compilerwarnings and errors
+    """
+    try:
+        result = subprocess.run(["poetry", "run", "python", "-m", "py_compile", filepath], capture_output=True, text=True)
+        return {"success": True, "message": "OK", "data": result.stdout}
+    except Exception as e:
+        return {"success": False, "message": str(e), "data": None}
