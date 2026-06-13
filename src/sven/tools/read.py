903a8bc4c5b84ae3d8f2ca590ebd43bb156d8245
Josef Schulz <j.schulz@alaun.de>
2026-06-13 23:05:43 +0200

    offset based read

--- src/sven/tools/read.py
+++ src/sven/tools/read.py
@@ -1,12 +1,23 @@
 import subprocess
 
-def read(filepath: str) -> dict:
+def read(filepath: str, offset: int = 0) -> dict:
     """
     Read the content of a file.
+    Max size of the data is 2048 bytes, based on the offset.
+
+    args:
+        str: filepath
+        int: offset (default is 0)
+
+    returns:
+        dict: success status, message, and data
     """
+    max_size = 2048
+
     try:
         with open(filepath, 'r', encoding='utf-8') as file:
-            data = file.read()
+            file.seek(offset)
+            data = file.read(max_size)
         return {"success": True, "message": "OK", "data": data}
     except Exception as e:
         return {"success": False, "message": str(e), "data": None}
