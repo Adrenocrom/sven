a003351f3fc9514a8f6309a3aa01100bf04ada2d
Josef Schulz <j.schulz@alaun.de>
2026-06-18 10:53:45 +0200

    u

---
+++ src/sven/__init__.py
@@ -0,0 +1,7 @@
+"""
+Sven package – core utilities.
+
+The :class:`~sven.token_counter.TokenCounter` class is exposed at the top level for convenience.
+"""
+
+from .token_counter import TokenCounter  # noqa: F401
