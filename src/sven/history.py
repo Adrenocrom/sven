b3ab391976f10211c912738ecf222ba16eaef93b
Josef Schulz <j.schulz@alaun.de>
2026-06-19 13:05:19 +0200

    U

--- src/sven/history.py
+++ src/sven/history.py
@@ -1,9 +1,10 @@
 import json
 from pathlib import Path
+from sven import Config
 
-def load_history() -> list[str]:
+def load_history(config: Config) -> list[str]:
     try:
-        file_path = Path('history.json')
+        file_path = Path(os.path.join(config.data_dir, "chat_history.json"))
         if not file_path.exists():
             return []
         with open(file_path, 'r') as file:
@@ -13,10 +14,10 @@ def load_history() -> list[str]:
         print(f"Error loading history: {e}")
         return []
 
-def store_history(history: list[str]):
+def store_history(config: Config, history: list[str]):
     try:
         final_history = [m for m in history if not m.get('tool_calls')] 
-        file_path = Path('history.json')
+        file_path = Path(config.data_dir, "chat_history.json")
         with open(file_path, 'w') as file:
             json.dump(final_history, file, indent=4)
     except Exception as e:
