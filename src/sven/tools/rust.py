1730bac2293f8030ddc19fd107ad76283ef86105
Josef Schulz <j.schulz@alaun.de>
2026-07-13 08:25:57 +0200

    U

--- src/sven/tools/rust.py
+++ src/sven/tools/rust.py
@@ -8,6 +8,6 @@ def cargoBuild() -> dict:
     """
     try:
         result = subprocess.run(["cargo", "build"], capture_output=True, text=True)
-        return {"success": True, "message": "OK", "data": result.stdout}
+        return {"success": True, "message": "OK", "data": result}
     except Exception as e:
         return {"success": False, "message": str(e), "data": None}
